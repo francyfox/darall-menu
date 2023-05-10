81280411ddf0ad702e171ea00d5311e4dacd2812
hellisart <7info7web@gmail.com>
2023-05-10 17:14:56 +0600

    pong func

--- app/routes/api/ping/index.ts
+++ app/routes/api/ping/index.ts
@@ -2,6 +2,6 @@ import { Application, NextFunction, Request, Response } from 'express'
 import { Resource } from 'express-automatic-routes'
 export default (express: Application) => <Resource> {
   get: (request: Request, response: Response, next: NextFunction) => {
-    response.status(200).send('Hello, Route!').end()
+    response.status(200).send('Pong! 🏓').end()
   }
 }
