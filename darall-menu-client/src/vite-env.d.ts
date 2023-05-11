8d7e56c582a2cd422eeeb0fae0522e5dd6a82f43
hellisart <7info7web@gmail.com>
2023-05-11 16:31:56 +0600

    remove client from prod

--- darall-menu-client/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  readonly VITE_STORAGE_SECRET: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
