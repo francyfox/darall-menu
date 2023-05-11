0f98f829703c83d79037f38784a779f82011f281
hellisart <7info7web@gmail.com>
2023-05-11 16:06:45 +0600

    vercel add static

--- dist/app/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (express) { return ({
-    get: function (request, response, next) {
-        response.status(200).send('hello').end();
-    }
-}); });
--- src/app/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Application, NextFunction, Request, Response } from 'express'
-import { Resource } from 'express-automatic-routes'
-export default (express: Application) => <Resource> {
-  get: (request: Request, response: Response, next: NextFunction) => {
-    response.status(200).send('hello').end()
-  }
-}
